2a3e5d7d50b03f75e51a5be676deefbfb6888fcc
Tomi Malamud <tomasmalamud@gmail.com>
2024-04-08 14:58:17 -0300

    sitemap

---
+++ sitemap.ts
@@ -0,0 +1,25 @@
+// app/sitemap.ts
+
+import { MetadataRoute } from 'next';
+import { allPosts } from "@/.contentlayer/generated";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://rohisommiers.com';
+
+  const blogUrls = allPosts.map(post => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: new Date(post.date), 
+    changeFrequency: 'daily',
+    priority: 0.8,
+  }));
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    ...blogUrls,    
+  ];
+}
