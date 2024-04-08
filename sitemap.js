4140afa9f67c658d12c60ad6a000c2db738c6219
Tomi Malamud <tomasmalamud@gmail.com>
2024-04-08 16:51:25 -0300

    sitemap js base

--- sitemap.js
+++ sitemap.js
@@ -1,22 +1,10 @@
-import { allPosts } from "@/.contentlayer/generated";
-
 export default function sitemap() {
-  const baseUrl = 'https://blog.rohisommiers.com';
-
-  const blogUrls = allPosts.map(post => ({
-    url: `${baseUrl}/posts/${post.slug}`,
-    lastModified: new Date(post.date), 
-    changeFrequency: 'daily',
-    priority: 0.8,
-  }));
-
   return [
     {
-      url: baseUrl,
+      url: 'https://blog.rohisommiers.com',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
-    ...blogUrls,
   ]
 }
