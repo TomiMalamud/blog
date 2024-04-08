a03df6fd80ec2365fe9367eb2657fab8203c6dff
Tomi Malamud <tomasmalamud@gmail.com>
2024-04-08 18:54:31 -0300

    basico sitemap

--- sitemap.js
+++ sitemap.js
@@ -1,17 +1,8 @@
-import { allPosts } from "contentlayer/generated";
- 
-const URL = "https://blog.rohisommiers.com";
- 
-export default async function sitemap() {
-  const posts = allPosts.map(({ id, date }) => ({
-    url: `${URL}/blog/${id}`,
-    lastModified: date,
-  }));
- 
-  const routes = [""].map((route) => ({
-    url: `${URL}${route}`,
-    lastModified: new Date().toISOString(),
-  }));
- 
-  return [...routes, ...posts];
+export default function sitemap() {
+  return [
+    {
+      url: 'https://blog.rohisommiers.com',
+      lastModified: new Date(),
+    }
+  ]
 }
